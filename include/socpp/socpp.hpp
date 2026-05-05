f891fe5bfba6fd593babda38949f7298637365ca
Davis Reis <davisreis1@gmail.com>
2026-05-05 13:28:33 -0700

    added function declarations and private variables

---
+++ include/socpp/socpp.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+namespace socpp {
+
+class Socket {
+public:
+    Socket() noexcept = default;
+    explicit Socket(int fd) noexcept;
+    ~Socket();
+    
+    // Copy prevention
+    Socket(const Socket&) = delete;
+    Socket& operator=(const Socket&) = delete;
+
+    // Move constructor
+    Socket(Socket&& other) noexcept;
+    Socket& operator=(Socket&& other) noexcept;
+
+    bool valid() const noexcept;
+
+    int fd() const noexcept;
+
+    void close() noexcept;
+
+    
+
+private:
+    int fd_ = -1
+}
+
+} // namespace socpp
