0c5d62ada5884724cf54f7e10b942172ce7bf24e
Davis Reis <davisreis1@gmail.com>
2026-05-24 17:39:25 -0600

    readded private variable usage to non-static function

--- src/socket.cpp
+++ src/socket.cpp
@@ -138,7 +138,7 @@ Socket Socket::listen(std::uint16_t port) {
 }
 
 Socket Socket::accept() {
-    int client_fd = ::accept(, nullptr, nullptr);
+    int client_fd = ::accept(fd_ , nullptr, nullptr);
 
     if (client_fd != 0) {
         throw ConnectionError("accept failed: errno = " + std::string(std::strerror(errno)));
